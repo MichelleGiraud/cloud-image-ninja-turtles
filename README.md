5d3e2f91a69cd70518e24acdd2fd5580277b9f78
Sally Erisman <sally.erisman@ingka.ikea.com>
2023-09-27 08:13:15 +0200

    Set up the basic frontend.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# cloud-image-ninja-turtles
+# Hack days project: File upload to GCP
+
+Group name: Cloud Image Ninja Turtles
+
+Group participants: Michelle, Pernilla, Sally, Zack.
+
+Project description: We want to create a solution for uploading files/images to a GCP storage bucket. The images need to be scanned for malware using ClamAV, which runs in a Docker container in Cloud Run. We want to use Terraform to manage our GCP services.
---
+++ frontend/src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+function App() {
+  const [url, setUrl] = useState<string>("");
+  const [file, setFile] = useState<any>(null);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    let formData = new FormData();
+    formData.append("file", file.data);
+
+    const response = await fetch(
+      //TO DO:
+      //What URL should be here?
+      "http://localhost:5001/upload-file-to-cloud-storage",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    const responseWithBody = await response.json();
+    if (response) setUrl(responseWithBody.publicUrl);
+  };
+
+  const handleFileChange = (e: any) => {
+    const fileToUpload = {
+      data: e.target.files[0],
+    };
+    setFile(fileToUpload);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" name="file" onChange={handleFileChange}></input>
+      <button type="submit">Upload</button>
+    </form>
+  );
+}
+export default App;
