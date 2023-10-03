7d3789bbd8b272f1c85fb5b0ee12e096176d6f8a
Pernilla <pernilla.lundahl@ingka.ikea.com>
2023-10-03 10:16:31 +0200

    Restarted terraform and made some temporary changes to stop error logs

--- backend/cloudRunN.js
+++ backend/cloudRunN.js
@@ -1,61 +1,64 @@
 const express = require('express');
 const axios = require("axios");
 const app = express();
-const {Storage} = require('@google-cloud/storage');
 
 const port = process.env.PORT || 8080;
-const storage = new Storage();
 
 app.use(express.json());
 
-app.post('/', (req, res) => {
-  if (!req.body) {
-    const msg = 'no Pub/Sub message received';
-    console.error(`error: ${msg}`);
-    res.status(400).send(`Bad Request: ${msg}`);
-    return;
-  }
-  if (!req.body.message) {
-    const msg = 'invalid Pub/Sub message format';
-    console.error(`error: ${msg}`);
-    res.status(400).send(`Bad Request: ${msg}`);
-    return;
-  }
+// GETTING ERRORS (404) WHEN USING THIS FOR THE IMAGE
 
-  const pubSubMessage = req.body.message;
-  const data = Buffer.from(pubSubMessage.data, 'base64').toString().trim()
+app.get("/", (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
 
-  const sourceBucket = "cint_upload_bucket";
-  const sourceObject = JSON.parse(data.toString()).name;
+// app.post('/', (req, res) => {
+//   if (!req.body) {
+//     const msg = 'no Pub/Sub message received';
+//     console.error(`error: ${msg}`);
+//     res.status(400).send(`Bad Request: ${msg}`);
+//     return;
+//   }
+//   if (!req.body.message) {
+//     const msg = 'invalid Pub/Sub message format';
+//     console.error(`error: ${msg}`);
+//     res.status(400).send(`Bad Request: ${msg}`);
+//     return;
+//   }
 
-  const destinationBucket = "cint_clean_bucket"
-  const destinationObject = `clean-${sourceObject}`;
+//   const pubSubMessage = req.body.message;
+//   const data = Buffer.from(pubSubMessage.data, 'base64').toString().trim()
 
-  // apiUrl = `https://storage.googleapis.com/storage/v1/b/${sourceBucket}/o/${sourceObject}/rewriteTo/b/${destinationBucket}/o/${destinationObject}`;
+//   const sourceBucket = "cint_upload_bucket";
+//   const sourceObject = JSON.parse(data.toString()).name;
 
-  // console.log("URL:", apiUrl)
+//   const destinationBucket = "cint_clean_bucket"
+//   const destinationObject = `clean-${sourceObject}`;
 
-  const moveObject = async () => {
-    try {
-    //   const response = await axios.post(apiUrl)
-    //   if (response.status === 200) {
-    //     console.log("Success!");
-    //     res.status(200).send();
-    //   } else {
-      //     console.error("Bad response:", response.statusText)
-      //     res.status(response.status).send();
-      //   }
-      await storage.bucket(sourceBucket).file(sourceObject).move(destinationBucket)
-      console.log("Success!")
-    } catch (error) {
-      console.error("Failed:", error.message)
-    //   res.status(error.status).send();
-    }
+//   const apiUrl = `https://storage.googleapis.com/storage/v1/b/${sourceBucket}/o/${sourceObject}/rewriteTo/b/${destinationBucket}/o/${destinationObject}`;
 
-  }
+//   console.log("URL:", apiUrl)
 
-  moveObject();
- });
+//   const moveObject = async () => {
+//     try {
+//       const response = await axios.post(apiUrl)
+//       if (response.status === 200) {
+//         console.log("Success!");
+//         res.status(200).send();
+//       } else {
+//         console.error("Bad response:", response.statusText)
+//         res.status(response.status).send();
+//       }
+//     } catch (error) {
+//       console.error("Failed:", error.message)
+//       res.status(error.status).send();
+//     }
+
+//   }
+
+//   moveObject();
+//  });
 
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
--- terraform/main.tf
+++ terraform/main.tf
@@ -13,14 +13,14 @@ module "cint_clean_bucket" {
   bucket_name = "cint_clean_bucket"
 }
 
-resource "google_artifact_registry_repository" "cint-repo" {
+resource "google_artifact_registry_repository" "cint_repo" {
   repository_id = "cint-repo"
   format        = "DOCKER"
 }
 
-resource "google_artifact_registry_repository_iam_binding" "cint-repo-binding" {
-  location   = google_artifact_registry_repository.cint-repo.location
-  repository = google_artifact_registry_repository.cint-repo.name
+resource "google_artifact_registry_repository_iam_binding" "cint_repo_binding" {
+  location   = google_artifact_registry_repository.cint_repo.location
+  repository = google_artifact_registry_repository.cint_repo.name
   role       = "roles/artifactregistry.admin"
   members = [
     "user:sally.erisman@ingka.ikea.com",
@@ -31,12 +31,13 @@ resource "google_artifact_registry_repository_iam_binding" "cint-repo-binding" {
 }
 
 //Creates a Cloud Run service
-resource "google_cloud_run_v2_service" "cint-cloud-run" {
+resource "google_cloud_run_v2_service" "cint_cloud_run" {
   name     = "cint-cloud-run"
   location = var.location
   template {
     containers {
-      image = "europe-west1-docker.pkg.dev/ingka-native-ikealabs-dev/cint-repo/cint-my-docker-image:latest"
+      # image = "europe-west1-docker.pkg.dev/ingka-native-ikealabs-dev/cint-repo/cint-my-docker-image:latest"
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
     }
     service_account = google_service_account.cint_sa.email
     //environment_variables = {
@@ -54,7 +55,7 @@ resource "google_pubsub_topic" "cint_topic" {
 }
 
 //Right now has a lot of permissions maybe look into roles/pubsub.topic.____ instead of admin
-resource "google_pubsub_topic_iam_binding" "binding" {
+resource "google_pubsub_topic_iam_binding" "cint_topic_binding" {
   topic   = google_pubsub_topic.cint_topic.name
   role    = "roles/pubsub.admin"
   members = ["serviceAccount:${google_service_account.cint_sa.email}"]
@@ -65,12 +66,12 @@ resource "google_pubsub_subscription" "cint_subscription" {
   name  = "cint_subscription"
   topic = "cint_topic"
   push_config {
-    push_endpoint = google_cloud_run_v2_service.cint-cloud-run.uri
+    push_endpoint = google_cloud_run_v2_service.cint_cloud_run.uri
     oidc_token {
       service_account_email = google_service_account.cint_sa.email
     }
   }
-  depends_on = [google_cloud_run_v2_service.cint-cloud-run]
+  depends_on = [google_cloud_run_v2_service.cint_cloud_run]
 }
 
 //Creates a Google Service Account
@@ -80,9 +81,9 @@ resource "google_service_account" "cint_sa" {
 }
 
 //Binds the Cloud Run service to the Service Account
-resource "google_cloud_run_service_iam_binding" "binding" {
-  location = google_cloud_run_v2_service.cint-cloud-run.location
-  service  = google_cloud_run_v2_service.cint-cloud-run.name
+resource "google_cloud_run_service_iam_binding" "cint_service_binding" {
+  location = google_cloud_run_v2_service.cint_cloud_run.location
+  service  = google_cloud_run_v2_service.cint_cloud_run.name
   role     = "roles/run.admin"
   members  = ["serviceAccount:${google_service_account.cint_sa.email}"]
 }
@@ -103,23 +104,21 @@ resource "google_project_service_identity" "cint_pubsub_agent" {
   service  = "pubsub.googleapis.com"
 }
 
-resource "google_project_iam_binding" "project_token_creator" {
+resource "google_project_iam_binding" "cint_project_token_creator" {
   project = var.project_name
   role    = "roles/iam.serviceAccountTokenCreator"
-  members = ["serviceAccount:${google_project_service_identity.cint_pubsub_agent.email}",
-    "serviceAccount:${google_service_account.cint_sa.email}",
-  "serviceAccount:sa-creator@ingka-native-ikealabs-dev.iam.gserviceaccount.com", ]
+  members = ["serviceAccount:${google_project_service_identity.cint_pubsub_agent.email}"]
 }
 
 //PubSub notification
 
 // Create a Pub/Sub notification.
-resource "google_storage_notification" "notification" {
+resource "google_storage_notification" "cint_notification" {
   provider       = google-beta
   bucket         = module.cint_upload_bucket.bucket_name
   event_types    = ["OBJECT_FINALIZE"] # Trigger when a new object is created
   payload_format = "JSON_API_V1"
   topic          = google_pubsub_topic.cint_topic.id
-  depends_on     = [google_pubsub_topic_iam_binding.binding]
+  depends_on     = [google_pubsub_topic_iam_binding.cint_topic_binding]
 }
 
